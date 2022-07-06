144e7acbdb96c618bfe35c5ae924178eb765410c
Flávio Dinis de Oliveira Figueiredo Lopes da Silva <dinissilvauk@gmail.com>
2022-07-06 22:15:12 +0100

    Implement Firebase Auth + Database and connect

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCm098jjBLqIef_DMaplqa9xVB3I0IuinA",
+  authDomain: "project-test-8f06b.firebaseapp.com",
+  projectId: "project-test-8f06b",
+  storageBucket: "project-test-8f06b.appspot.com",
+  messagingSenderId: "499881558843",
+  appId: "1:499881558843:web:21782e5e7873a3dacaed1e",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (err) {
+      console.log("error creating the user", err.message);
+    }
+  }
+
+  return userDocRef;
+};
