9959024f383d4b039aa1144c41343b76b607f115
Flávio Dinis de Oliveira Figueiredo Lopes da Silva <dinissilvauk@gmail.com>
2022-07-15 20:34:38 +0100

    refactor categories to use ts

--- src/store/categories/categories.selector.ts
+++ src/store/categories/categories.selector.ts
@@ -1,7 +1,12 @@
 import { createSelector } from "reselect";
 
-const selectCategoryReducer = (state) => state.categories; // This is the first and only thing checked works like a cache
-//                                                       preventing the under functions to be executed when they don't need to
+import { CategoriesState } from "./categories.reducer";
+import { CategoryMap } from "./categories.types";
+
+const selectCategoryReducer = (state: {
+  categories: CategoriesState;
+}): CategoriesState => state.categories; // This is the first and only thing checked works like a cache
+//                                          preventing the under functions to be executed when they don't need to
 
 export const selectCategories = createSelector(
   [selectCategoryReducer], // another value added here, is added down too
@@ -10,11 +15,11 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) =>
+  (categories): CategoryMap =>
     categories.reduce((acc, { title, items }) => {
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
+    }, {} as CategoryMap)
 );
 
 export const selectCategoriesIsLoading = createSelector(
