472fefc07c07bcffa8c441450f2700f0de35ba3a
Flávio Dinis de Oliveira Figueiredo Lopes da Silva <dinissilvauk@gmail.com>
2022-07-16 19:21:38 +0100

    add global styles with createGlobalStyle and css feature in navigation.styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { useDispatch } from "react-redux";
 
 import { Routes, Route } from "react-router-dom";
 
+import { GlobalStyle } from "./global.styles";
+import { checkUserSession } from "./store/user/user.action";
+import Spinner from "./components/spinner/spinner.component";
+
 const Navigation = lazy(() =>
   import("./routes/navigation/navigation.component")
 );
@@ -13,9 +17,6 @@ const Authentication = lazy(() =>
   import("./routes/authentication/authentication.component")
 );
 
-import { checkUserSession } from "./store/user/user.action";
-import Spinner from "./components/spinner/spinner.component";
-
 const App = () => {
   const dispatch = useDispatch();
 
@@ -25,6 +26,7 @@ const App = () => {
 
   return (
     <Suspense fallback={<Spinner />}>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Navigation />}>
           <Route index element={<Home />} />
